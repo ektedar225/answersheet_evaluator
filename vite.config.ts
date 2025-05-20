3beca1b27836b430364fd9d4c3ee069b945af113
ektedar225 <ektedarahmad225@gmail.com>
2025-05-20 12:01:39 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/User_interface_for_handwritten_answer_sheet_evaluation/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
